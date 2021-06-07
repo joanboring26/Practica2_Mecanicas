e47d1d687688433206fdec25447e9877837f3017
Victorín <victorgarcinuno@gmail.com>
2021-06-07 11:26:15 +0200

    ComputeShader Script

---
+++ Assets/Scenes/Delivery2/ComputeShaders/ParticleEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public ComputeShader computeShader;
+    public RenderTexture renderTexture;
+    void Start()
+    {
+        renderTexture = new RenderTexture(256, 256, 24);
+        renderTexture.enableRandomWrite = true;
+        renderTexture.Create();
+
+        computeShader.SetTexture(0, "Result", renderTexture);
+        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
